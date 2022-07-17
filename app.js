50b88c9bf8ec523754ecd9fbac1e6fa3df27b6bf
vsl <vsl900@ukr.net>
2022-07-17 13:23:54 +0300

    Blog_1

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
+app.get('/', function (req, res) {
+    res.render('index', {activePage: "home"})   
+   })
+    
+   app.listen(3000)
+   
