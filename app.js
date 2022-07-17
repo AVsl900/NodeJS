fc5fbdf035d14945498cbd56786447dea888e5f4
vsl <vsl900@ukr.net>
2022-07-17 13:55:03 +0300

    Blog_Etalon

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'))
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
+
+app.use(express.urlencoded())
+
 app.get('/', function (req, res) {
-    res.render('index', {activePage: "home"})   
-   })
+    res.render('index', {activePage: "home"}) 
+}) 
+app.get('/contact', function (req, res) {
+        res.render('contact', {activePage: "contact"})
+})
+       
+app.post('/contact', function (req, res) {
+    res.render('contact_answer', {activePage: "contact", formData: req.body})
+})
+
     
+   
    app.listen(3000)
