f35fcede0ecc5a3507dc4297d4f78e10d9f822e4
vsl <vsl900@ukr.net>
2022-07-26 21:59:07 +0300

    blog_add21

--- app.js
+++ app.js
@@ -259,9 +259,13 @@ app.post('/new_post', function (req, res) {
     //res.redirect('/posts');
     })*/
 
-    app.get('/news/:id', function(req, res){
+    app.get('/news', function(req, res){
       let obj = { title:'Новость', id: 4, paragraphs:['Параграф', 'Обычный текст', 'Числа: 3, 7, 24', 476]};
       res.render('news', {newsId: req.params.id, newParam: 535, obj: obj});
+    });
+    app.get('/contact/:id', function(req, res){
+      let obj = { title:'Новость', id: 4, paragraphs:['Параграф', 'Обычный текст', 'Числа: 3, 7, 24', 476]};
+      res.render('contact', {activePage: "contact", newsId: req.params.id, newParam: 535, obj: obj});
     });
 
    app.listen(3000, function() {
