11aff6c013ddd44944356288eefa80475419b36b
vsl <vsl900@ukr.net>
2022-07-27 10:35:25 +0300

    Blog_25

--- sort_funcs.js
+++ sort_funcs.js
@@ -1,29 +1,21 @@
-/*
-function compareA(a, b)
-{
-    if (a.author > b.author)
-        return -1;
-    return 1;
-}
 
 function compareT(a, b)
 {
-    if (a.title > b.title)
-        return -1;
-    return 1;
+if (a.title < b.title)
+  return -1;
+if (a.title > b.title)
+return 1;
 }
 
-function sortA(){
-    flagSort = 'author';
-    //console.log("flagSort= " + flagSort);
-    } 
-    function sortT(){
-    flagSort = 'title';
-    //console.log("flagSort= " + flagSort);
-    }   
-
+function compareA(a, b)
+{
+  if (a.author < b.author)
+    {return -1;}
+  if (a.author > b.author)
+    {return 1; }
+  return 0;
+  
+}
+//module.exports = compareA;
 module.exports.compareA = compareA;
-module.exports.compareT = compareT;
-module.exports.sortA = sortA;
-module.exports.sortT = sortT;
-*/
+//module.exports.compareT = compareT;
